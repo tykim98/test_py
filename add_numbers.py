47d0ddedeb60b88c4e2269ed0c3a5d341db545b6
tykim98 <tykim98@gmail.com>
2026-04-23 10:14:40 +0900

    Updated add_numbers.py to change from addition to multiplication.

--- add_numbers.py
+++ add_numbers.py
@@ -1,8 +1,8 @@
-def add_numbers(num1, num2):
-    """This function takes two numbers as input and returns their sum."""
-    return num1 + num2
+def multiply_numbers(num1, num2):
+    """This function takes two numbers as input and returns their product."""
+    return num1 * num2
 
 if __name__ == '__main__':
     number1 = float(input("Enter first number: "))
     number2 = float(input("Enter second number: "))
-    print(f'The sum is: {add_numbers(number1, number2)}')
+    print(f'The product is: {multiply_numbers(number1, number2)}')
